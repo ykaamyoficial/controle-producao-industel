1215336bee1d890bc6f2c2bfd7b5821e664eb7a8
ykaamyoficial <heriquelar9l2voce@gmail.com>
2026-06-24 17:37:13 -0300

    Atualizar versao de distribuicao para 2.4.3

--- app/version.py
+++ app/version.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
 APP_NAME = "Controle de Producao Industel"
-APP_VERSION = "2.4.2"
-APP_BUILD = "2026.06.23"
+APP_VERSION = "2.4.3"
+APP_BUILD = "2026.06.24"
 APP_PUBLISHER = "Industel"
 APP_CHANNEL = "stable"
 APP_EXECUTABLE_NAME = "ControleProducao.exe"
